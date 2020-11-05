0fc4f1370eb4c42c2598dbbb29c1a212ad6a9c6e
AllysaM <allysamjohnson@students.abtech.edu>
2020-11-04 19:55:14 -0500

    Original commit for working with .gitignore

---
+++ database/config.php
@@ -0,0 +1,19 @@
+<?php
+/*
+/ database connection information
+*/
+if($_SERVER['HTTP_HOST'] == "allysaj.sgedu.site")
+{
+    define('SERVER', 'localhost' );
+    define('USER', 'u7dkat9hvtqmv' );
+    define('PW', 'RuleofNine' );
+    define('DB', 'dbau6j3tpamc7p' );
+}
+
+else
+{
+    define('SERVER', 'localhost' );
+    define('USER', 'u7dkat9hvtqmv' );
+    define('PW', 'RuleofNine' );
+    define('DB', 'dbau6j3tpamc7p' );
+}
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB 182</title>
+  </head>
+  <body>
+    <h1>WEB 182</h1>
+    <h2>Chapter 15 Files</h2>
+    <ul>
+      <li><a href="./config.php">config.php</a></li>
+      <li><a href="./fixit1.php/">fixit1.php</a></li>
+      <li><a href="./fixit2.php/">fixit2.php</a></li>
+      <li><a href="./fixit3.php/">fixit3.php</a></li>
+      <li><a href="./fixit4.php/">fixit4.php</a></li>
+      <li><a href="./fixit5.php/">fixit5.php</a></li>
+      <li><a href="modify1.html">modify1.html</a></li>
+      <li><a href="./modify1.php/">modify1.php</a></li>
+      <li><a href="modify2.html">modify2.html</a></li>
+      <li><a href="./modify2.php/">modify2.php</a></li>
+      <li><a href="modify3.html">modify3.html</a></li>
+      <li><a href="./modify3.php/">modify3.php</a></li>
+      <li><a href="./server-test.php">server-test.php</a></li>
+      <li><a href="paint-estimate.html">paint-estimate.html</a></li>
+      <li><a href="./paint-estimate.php/">paint-estimate.php</a></li>
+      <li><a href="software-order.html">software-order.html</a></li>
+      <li><a href="./software-order.php/">software-order.php</a></li>
+      <li><a href="./inc-employee-object.php/">inc-employee-object.php</a></li>
+    </ul>
+  </body>
+</html>
